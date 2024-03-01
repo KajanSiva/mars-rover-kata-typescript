1cbd413eefadc9249390be30752da3b9b490aff8
Kajan Siva <716149+KajanSiva@users.noreply.github.com>
2024-03-01 11:21:08 +0100

    feat: implement commands that move the rover forward/backward

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -11,4 +11,24 @@ describe('Rover', () => {
     expect(rover.coordinates).toEqual(startingCoordinates);
     expect(rover.direction).toEqual(direction);
   });
+
+  it('should move the rover forward', () => {
+    const startingCoordinates = { x: 0, y: 0 };
+    const direction = 'N';
+
+    const rover = new Rover(startingCoordinates, direction);
+    rover.processCommands(['f']);
+
+    expect(rover.coordinates).toEqual({ x: 0, y: 1 });
+  });
+
+  it('should move the rover backward', () => {
+    const startingCoordinates = { x: 0, y: 0 };
+    const direction = 'N';
+
+    const rover = new Rover(startingCoordinates, direction);
+    rover.processCommands(['b']);
+
+    expect(rover.coordinates).toEqual({ x: 0, y: -1 });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ type Coordinates = {
   x: number;
   y: number;
 };
+type Command = 'f' | 'b';
 
 export class Rover {
   coordinates: Coordinates;
@@ -11,5 +12,64 @@ export class Rover {
   constructor(startingCoordinates: Coordinates, direction: Direction) {
     this.coordinates = startingCoordinates;
     this.direction = direction;
+  }
+
+  processCommands(commands: Command[]) {
+    for (let i = 0; i < commands.length; i++) {
+      this.#processCommand(commands[i]);
+    }
+  }
+
+  #processCommand(command: Command) {
+    switch (command) {
+      case 'f':
+        this.#moveForward();
+        break;
+      case 'b':
+        this.#moveBackward();
+        break;
+      default:
+        throw new Error('Invalid command');
+    }
+  }
+
+  #moveBackward() {
+    this.#move(true);
+  }
+
+  #moveForward() {
+    this.#move(false);
+  }
+
+  #move(isMovingBackward: boolean) {
+    let xOffset = 0;
+    let yOffset = 0;
+
+    switch (this.direction) {
+      case 'N':
+        yOffset = 1;
+        break;
+      case 'S':
+        yOffset = -1;
+        break;
+      case 'E':
+        xOffset = 1;
+        break;
+      case 'W':
+        xOffset = -1;
+        break;
+      default:
+        throw new Error('Invalid direction');
+    }
+
+    if (isMovingBackward) {
+      xOffset = xOffset * -1;
+      yOffset = yOffset * -1;
+    }
+
+    this.coordinates = {
+      x: this.coordinates.x + xOffset,
+      y: this.coordinates.y + yOffset,
+    };
   }
 }
